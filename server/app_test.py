2ab2fd9d4f917fcbf24074dfd67cda70ffd08a4d
Kenneth Harmon <KennethRHarmon@gmail.com>
2022-11-14 12:57:57 +0000

    Fixed test (for real this time)

--- server/app_test.py
+++ server/app_test.py
@@ -11,7 +11,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get_hello_endpoint(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, b'Hello World!')
+        self.assertEqual(r.data, b'Hello world!')
 
 if __name__ == '__main__':
     unittest.main()
