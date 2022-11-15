2085f9f6c0e57e3a271fd0f87274f8c29eb3d213
HaoyuWww <79248601+HaoyuWww@users.noreply.github.com>
2022-11-15 21:10:42 +0800

    Update app.py

--- server/app.py
+++ server/app.py
@@ -6,6 +6,11 @@ app =  Flask(__name__)
 def index():
     return "Hello world!"
 
+@app.route("/username")
+def user():
+    data = 
+    return data,200,{"ContentType":"application/json"}
+
 if __name__ == "__main__":
     app.run(debug=True)
 
