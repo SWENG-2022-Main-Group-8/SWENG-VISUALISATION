5c0ab3d492bc34ba4c3e0590c0e6b99604c798a3
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-11-16 16:08:13 +0000

    Made some changes by adding a post method

--- server/app.py
+++ server/app.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 import backEnd as bk
+from flask_cors import CORS
+import json
 
-app =  Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return "Hello world!"
 
-@app.route("/username")
-def user():
-    data = bk.re_to_json()
-    return data,200,{"ContentType":"application/json"}
+@app.route('/username', methods=["GET", "POST"])
+def users():
+    print("users endpoint reached...")
+    if request.method == "GET":
+        with open("users.json", "r") as f:
+            data = json.load(f)
+            data.append({
+                "username": "user4",
+                "pets": ["hamster"]
+            })
+            return Flask.jsonify(data)
+    if request.method == "POST":
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        username = received_data['data']
+        data = bk.re_to_json(username)
+        return Flask.Response(response=json.dumps(data), status=201)
 
 if __name__ == "__main__":
     app.run(debug=True)
