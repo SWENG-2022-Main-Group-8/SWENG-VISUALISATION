54abd2ea29c707be13996c592b7acc536e06482a
HaoyuWww <79248601+HaoyuWww@users.noreply.github.com>
2022-11-16 21:28:31 +0800

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import back-end.backEnd as bk
 
 app =  Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello world!"
+
+@app.route("/username")
+def user():
+    data = bk.re_to_json()
+    return data,200,{"ContentType":"application/json"}
 
 if __name__ == "__main__":
     app.run(debug=True)
