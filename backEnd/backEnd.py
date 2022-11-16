9374d1cc05ede5f86001098d80c872881e7ae11c
HaoyuWww <79248601+HaoyuWww@users.noreply.github.com>
2022-11-16 13:55:12 +0000

    Create backEnd.py

---
+++ backEnd/backEnd.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+username = input("Enter username: ")
+url = "https://api.github.com/users/{}".format(username)
+
+def re_to_json():
+  response = requests.get(url)
+  print(json.loads(response.text))
+  return json.loads((response.text))
