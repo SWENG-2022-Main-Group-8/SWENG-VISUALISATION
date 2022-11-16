24d8904096c4613f7ed0c5e5f1c325e60e370b48
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-11-16 13:45:46 +0000

    Basic protocol recieving

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 import backEnd
 import json
+from flask_cors import CORS
 
-app =  Flask(__name__)
 
+app = Flask(__name__)
+CORS(app)
 
 @app.route("/username", methods=["GET", "POST"])
 def user():
