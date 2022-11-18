14e2575c0c68dc35dfae722646dab1313df0efdd
OmaidQ <qureshmu@tcd.ie>
2022-11-18 05:56:52 +0000

    Implemntation of retrieving language data from API
    Made multiple API calls to retrieve language data from Github's API. Number of lines of code written for each language and a counter for how many times a language is used for all public repositories are stored in the language dictionary

--- server/app.py
+++ server/app.py
@@ -1,13 +1,12 @@
-
 from flask import Flask, request
 import flask
 from library import username as usernameAPI
+from library import languageData as languagesAPI
 from flask_cors import CORS
 import json
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/')
 def index():
@@ -16,17 +15,22 @@ def index():
 @app.route('/username', methods=["GET", "POST"])
 def users():
     print("users endpoint reached...")
+
     if request.method == "GET":
-        with open("server\OmaidQ.json", "r") as f:
+        languages = languagesAPI.retrieveLanguages("OmaidQ")
+        with open("OmaidQ.json", "r") as f:
             data = json.load(f)
-            return flask.jsonify(data)
+            merge = dict(data.items() | languages.items())
+            return flask.jsonify(merge)
+
     if request.method == "POST":
         received_data = request.get_json()
         print(f"received data: {received_data}")
         username = received_data['data']
-        data = usernameAPI.re_to_json(username)
-        return flask.Response(response=json.dumps(data), status=201)
-
+        userInfo = usernameAPI.userInfo(username)
+        languages = languagesAPI.retrieveLanguages(username)
+        mergeOfDataAndLanguages = dict(userInfo.items() | languages.items())
+        return flask.Response(response=json.dumps(mergeOfDataAndLanguages), status=201)
 
 if __name__ == "__main__":
     app.run(debug=True)
