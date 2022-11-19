767219a0a9201edcf05b50b161185f23126ba4b2
AmmarMgmv <ammarmgmv@gmail.com>
2022-11-19 00:31:27 +0000

    Updated card animation

--- front-end/app.js
+++ front-end/app.js
@@ -155,17 +155,28 @@ function draw1(ctx, type, datasetLabel, titleText, label, data, backgroundColor)
 
 var chart1 = null;
 
-const handleOnMouseMove = e => {
-    const {currentTarget: target} = e;
+// const handleOnMouseMove = e => {
+//     const {currentTarget: target} = e;
 
-    const rect = target.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
+//     const rect = target.getBoundingClientRect(),
+//         x = e.clientX - rect.left,
+//         y = e.clientY - rect.top;
 
-    target.style.setProperty("--mouse-x", `${x}px`)
-    target.style.setProperty("--mouse-y", `${y}px`)
-}
+//     target.style.setProperty("--mouse-x", `${x}px`)
+//     target.style.setProperty("--mouse-y", `${y}px`)
+// }
 
-for(const card of document.querySelectorAll(".card")){
-    card.onmousemove = e => handleOnMouseMove(e);
+// for(const card of document.querySelectorAll(".card")){
+//     card.onmousemove = e => handleOnMouseMove(e);
+// }
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")){
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`)
+        card.style.setProperty("--mouse-y", `${y}px`)
+    }
 }
