5a02c161d6662b8d4d37d62dc7cef5af8c2123c7
AmmarMgmv <ammarmgmv@gmail.com>
2022-11-19 01:17:00 +0000

    Removing unnecessary comments

--- front-end/app.js
+++ front-end/app.js
@@ -155,21 +155,6 @@ function draw1(ctx, type, datasetLabel, titleText, label, data, backgroundColor)
 
 var chart1 = null;
 
-// const handleOnMouseMove = e => {
-//     const {currentTarget: target} = e;
-
-//     const rect = target.getBoundingClientRect(),
-//         x = e.clientX - rect.left,
-//         y = e.clientY - rect.top;
-
-//     target.style.setProperty("--mouse-x", `${x}px`)
-//     target.style.setProperty("--mouse-y", `${y}px`)
-// }
-
-// for(const card of document.querySelectorAll(".card")){
-//     card.onmousemove = e => handleOnMouseMove(e);
-// }
-
 document.getElementById("cards").onmousemove = e => {
     for(const card of document.getElementsByClassName("card")){
         const rect = card.getBoundingClientRect(),
