2a545509e40689e2d7325a1cfaa54d051caae70a
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-11-25 15:20:03 +0000

    Working for a org of any size but still not working for italy

--- server/library/repo_maps.py
+++ server/library/repo_maps.py
@@ -1,47 +1,50 @@
 import requests
 import json
-from geopy.geocoders import Nominatim
-from geopy.geocoders import GoogleV3
+from itertools import takewhile
 
-
-def getOrgData(organisation):
+def getOrgLocationData(organisation):
     countryDict = {}
+    #   Do until api fails
+    
+    url = ("https://api.github.com/orgs/{}/members?page=1".format(organisation))
+    res=requests.get(url)
+    orgData=res.json()
 
-#   Do until api fails
-    for i in range(1):
-        responseOrg = requests.get("https://api.github.com/orgs/{}/members?page40".format(organisation,i))
-        orgData = responseOrg.json()
+
+    while 'next' in res.links.keys():
+        res=requests.get(res.links['next']['url'])
+        orgData.extend(res.json())
         
-        print(orgData)
-        for i in orgData:
-            username = i["login"]
-            # print(username)
-            try:
-                response = (requests.get("https://api.github.com/users/" + username))
-            except requests.exceptions.RequestException as e:
-                continue
-            userData = response.json()
-            location =userData["location"]
-            if location != "None":
-                response = requests.get("https://nominatim.openstreetmap.org/search?q={}&format=json&addressdetails=1&limit=1".format(location))
-                responseJsonFile = response.json()
-                print("")
-                print(location)
-                print("country code : "+responseJsonFile[0]['address']['country_code'])
-                countryCode = responseJsonFile[0]['address']['country_code']
+    print(orgData)
+    for i in orgData:
+        username = i["login"]
+        # print(username)
+        try:
+            response = requests.get("https://api.github.com/users/" + username)
+        except requests.exceptions.RequestException as e:
+            continue
+        userData = response.json()
+        location =userData["location"]
+        if location != "None":
+            response = requests.get("https://nominatim.openstreetmap.org/search?q={}&format=json&addressdetails=1&limit=1".format(location))
+            responseJsonFile = response.json()
+            print("")
+            print(location)
+            print("country code : "+responseJsonFile[0]['address']['country_code'])
+            countryCode = responseJsonFile[0]['address']['country_code']
 
-                try:                    
-                    countryDict[countryCode] = countryDict[countryCode]+1
-                except KeyError:
-                    countryDict[countryCode] = 1
-
+            try:                    
+                countryDict[countryCode] = countryDict[countryCode]+1
+            except KeyError:
+                countryDict[countryCode] = 1
+# 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
                 
     print(countryDict)
     return json.dumps(countryDict, indent = 4)
-
+    
 
         
-getOrgData("facebook")
+getOrgLocationData("camunda")
 
 
 # geolocator = Nominatim(user_agent="Git Repo Mapper")
